c2f4ffe06a6d301caa854e960728d2ba43e78083
mbmande <mbsmande@gmail.com>
2024-11-28 23:10:57 +0000

    making changes commit

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""=================="""
+""" Contains makeChange function"""
+
+
+def makeChange(coins, total):
+    """
+    Returns: ==========================
+    """
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
+        return 0
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
