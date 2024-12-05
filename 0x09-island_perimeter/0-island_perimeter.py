7cb3273ba2e152da5fe72b15be596b7f36eb34ab
mbmande <mbsmande@gmail.com>
2024-12-05 20:26:52 +0000

    parimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
+""" ========================== """
+
 def island_perimeter(grid):
+    """
+    Input: =========
+    Returns: ===========
+    """
     p = 0
+
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if (grid[i][j] == 1):
