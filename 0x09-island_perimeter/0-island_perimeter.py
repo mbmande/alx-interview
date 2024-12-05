75ef0e63b659528b6d970c718248a6031ccd4486
mbmande <mbsmande@gmail.com>
2024-12-05 20:11:28 +0000

    perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 
 def island_perimeter(grid):
-
-    count = 0
-    row = len(grid)
-    col = len(grid[0]) if row else 0
-
+    p = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-
-            idx = [(i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)]
-            check = [1 if k[0] in range(row) and k[1] in range(col) else 0
-                     for k in idx]
-
-            if grid[i][j]:
-                count += sum([1 if not r or not grid[k[0]][k[1]] else 0
-                              for r, k in zip(check, idx)])
-
-    return (count)
+            if (grid[i][j] == 1):
+                if (i <= 0 or grid[i - 1][j] == 0):
+                    p += 1
+                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
+                    p += 1
+                if (j <= 0 or grid[i][j - 1] == 0):
+                    p += 1
+                if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
+                    p += 1
+    return p
